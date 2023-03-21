ab4531074537a549ab2f1033522eb44513a85137
Abbas Srour <abbas.mj.srour@gmail.com>
2023-03-22 00:23:48 +0200

    reset env

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,8 +2,6 @@ from rest_framework import generics, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from responses.models import ResponseModel
-from user.major_ml import y_pred
 from user.models import UserModel
 from user.serializer import UserSerializer
 
@@ -25,13 +23,13 @@ class MlMajorView(generics.GenericAPIView):
         except UserModel.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        responses = ResponseModel.objects.filter(user=user)
-        response_array = []
-
-        for response in responses:
-            response_array.append(response)
-
-        major = y_pred(response_array)
-        print(major)
+        # responses = ResponseModel.objects.filter(user=user)
+        # response_array = []
+        #
+        # for response in responses:
+        #     response_array.append(response)
+        #
+        # major = y_pred(response_array)
+        # print(major)
 
         return Response(status=status.HTTP_200_OK)
